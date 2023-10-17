63c9dcbcfd09dd40c504f28dbf9e1da91c713d7b
Jeremy Castle <x95castle1@yahoo.com>
2023-10-17 14:08:49 -0500

    JC38

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + JC37", controller.index());
+        assertEquals("Greetings from Spring Boot + JC38", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + JC37"));
+            .andExpect(content().string("Greetings from Spring Boot + JC38"));
     }
 }
