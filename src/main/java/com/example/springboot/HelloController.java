c714dfb97342e4c6682447048dd2de79628d995a
Jeremy Castle <x95castle1@yahoo.com>
2023-10-17 12:50:13 -0500

    JC13

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + JC12";
+		return "Greetings from Spring Boot + Tanzu + JC13";
 	}
 
 }
--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + JC12", controller.index());
+        assertEquals("Greetings from Spring Boot + JC13", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + JC12"));
+            .andExpect(content().string("Greetings from Spring Boot + JC13"));
     }
 }
