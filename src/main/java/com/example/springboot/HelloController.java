c808ebcd8fbbf2f200723491003dd7839447debe
Jeremy Castle <x95castle1@yahoo.com>
2023-10-17 13:53:38 -0500

    JC31

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + JC30";
+		return "Greetings from Spring Boot + Tanzu + JC31";
 	}
 
 }
