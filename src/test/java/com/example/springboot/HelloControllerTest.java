d7bea68cf8d0fcb5e81a631298b0569585122bbd
Mae Large <mlarge@broadcom.com>
2024-05-02 11:06:48 -0700

    rev 13

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu + JC18", controller.index());
+        assertEquals("Greetings from Spring Boot + Tanzu + JC19", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu + JC18"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu + JC19"));
     }
 }
