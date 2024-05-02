9b370342f04d7fe33bf1f479a3a315c8db2bd2a2
Mae Large <mlarge@broadcom.com>
2024-05-02 11:06:10 -0700

    rev 11

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + JC16";
+		return "Greetings from Spring Boot + Tanzu + JC17";
 	}
 
 }
