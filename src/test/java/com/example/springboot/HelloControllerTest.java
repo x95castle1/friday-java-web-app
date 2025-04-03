07dd533d88b394f8d1c47ca181b8298f5488e862
Jeremy Castle <x95castle1@yahoo.com>
2025-04-03 09:16:17 -0500

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -11,7 +11,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-
 @WebMvcTest(HelloController.class)
 class HelloControllerTest {
 
